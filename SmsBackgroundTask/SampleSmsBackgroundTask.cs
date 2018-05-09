32d0e9633eab0dba84275fcc3d8f337a9d2cbf21
Nico Zhu <v-zhumin@microsoft.com>
2018-05-09 13:49:55 +0800

    Add project files.

---
+++ SmsBackgroundTask/SampleSmsBackgroundTask.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.Devices.Sms;
+using Windows.UI.Notifications;
+
+namespace SmsBackgroundTask
+{
+    public sealed class SampleSmsBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferal = taskInstance.GetDeferral();
+            taskInstance.Canceled += new BackgroundTaskCanceledEventHandler(OnCanceled);
+            DisplayToast(taskInstance);
+            deferal.Complete();
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            var settings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            settings.Values[sender.Task.TaskId.ToString()] = "Canceled";
+            Debug.WriteLine("Background " + sender.Task.Name + " Cancel Requested...");
+        }
+
+        private void DisplayToast(IBackgroundTaskInstance taskInstance)
+        {
+            try
+            {
+                SmsMessageReceivedTriggerDetails smsDetails = taskInstance.TriggerDetails as SmsMessageReceivedTriggerDetails;
+                SmsTextMessage2 smsTextMessage;
+
+                if (smsDetails.MessageType == SmsMessageType.Text)
+                {
+                    smsTextMessage = smsDetails.TextMessage;
+
+                    XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+                    XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+                    stringElements.Item(0).AppendChild(toastXml.CreateTextNode(smsTextMessage.From));
+                    stringElements.Item(1).AppendChild(toastXml.CreateTextNode(smsTextMessage.Body));
+
+                    ToastNotification notification = new ToastNotification(toastXml);
+                    ToastNotificationManager.CreateToastNotifier().Show(notification);
+                }
+                // Message ACK to operator
+                smsDetails.Accept();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error displaying toast: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ SmsTest/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Sms;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SmsTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private const string BackgroundTaskEntryPoint = "SmsBackgroundTask.SampleSmsBackgroundTask";
+        private const string BackgroundTaskName = "SampleSmsBackgroundTask";
+
+        private async void Register_BackgroundTask()
+        {
+            var settings = ApplicationData.Current.LocalSettings;
+            try
+            {
+                var access = await BackgroundExecutionManager.RequestAccessAsync();
+                switch (access)
+                {
+                    case BackgroundAccessStatus.Unspecified:
+                        break;
+                    case BackgroundAccessStatus.AlwaysAllowed:
+                        break;
+                    case BackgroundAccessStatus.AllowedSubjectToSystemPolicy:
+                        break;
+                    case BackgroundAccessStatus.DeniedBySystemPolicy:
+                        break;
+                    case BackgroundAccessStatus.DeniedByUser:
+                        break;
+                    default:
+                        break;
+                }
+
+                SmsMessageType _messageType = SmsMessageType.Text; // set as Text as default
+                _messageType = SmsMessageType.Text;
+
+                SmsFilterRule _filterRule = new SmsFilterRule(_messageType);
+
+
+                SmsFilterActionType _type = SmsFilterActionType.Accept;
+                _type = SmsFilterActionType.Accept;
+
+
+                SmsFilterRules _Rules = new SmsFilterRules(_type);
+
+                IList<SmsFilterRule> rules = _Rules.Rules;
+
+                rules.Add(_filterRule);
+
+                BackgroundTaskBuilder taskBuilder = new BackgroundTaskBuilder();
+                SmsMessageReceivedTrigger trigger = new SmsMessageReceivedTrigger(_Rules);
+
+                taskBuilder.SetTrigger(trigger);
+                taskBuilder.TaskEntryPoint = BackgroundTaskEntryPoint;
+                taskBuilder.Name = BackgroundTaskName;
+
+                foreach (var cur in BackgroundTaskRegistration.AllTasks)
+                {
+                    if (cur.Value.Name == BackgroundTaskName)
+                    {
+                        return;
+                    }
+                }
+
+                BackgroundTaskRegistration taskRegistration = taskBuilder.Register();
+
+                taskRegistration.Completed += new BackgroundTaskCompletedEventHandler(OnCompleted);
+
+                //LOG              
+            }
+            catch (Exception ex)
+            {
+                //LOG               
+            }
+        }
+
+        private void OnCompleted(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Register_BackgroundTask();
+        }
+    }
+}
